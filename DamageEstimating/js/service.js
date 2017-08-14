097f474dfd5049357c2c2695548b2cf296abad75
gone-with-the-windgit config --global user.email 491883290@qq.comgit config --global --global user.name gone-with-the-wind <491883290@qq.com>
2017-08-14 19:39:56 +0800

    update server.js

--- DamageEstimating/js/service.js
+++ DamageEstimating/js/service.js
@@ -1,7 +1,7 @@
 angular.module('MyService',[])
 	.service('urlService', function(){
      	this.getCommonUrl = function(){
-        return "http://localhost:8080/aaacccDamageEstimating-1.0-SNAPSHOT";
+        return "http://localhost:8080/DamageEstimating-1.0-SNAPSHOT";
      }
 })
 
